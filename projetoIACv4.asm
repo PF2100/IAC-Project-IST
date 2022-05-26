e31db686fe6e2dc5bad799ccd8408e6003ea4d7f
johny <jpmtrocado7@gmail.com>
2022-05-26 16:25:51 +0100

    acabei a indentação e double spaces

--- projetoIACv4.asm
+++ projetoIACv4.asm
@@ -1,11 +1,11 @@
 
 
 ;****KEYPAD****************************************************************************
-LINE 		EQU 8		; Fourth keypad line
-KEY_LIN 	EQU 0C000H	; Keyboard Rows
-KEY_COL 	EQU 0E000H	; Keyboard Columns
-KEY_MASK	EQU 0FH		; Isolates the lower nibble from the output of the keypad
-BUTTON		EQU 0900H   	; Memory address that stores the pressed button
+LINE 		EQU 8			; Fourth keypad line
+KEY_LIN 	EQU 0C000H		; Keyboard Rows
+KEY_COL 	EQU 0E000H		; Keyboard Columns
+KEY_MASK	EQU 0FH			; Isolates the lower nibble from the output of the keypad
+BUTTON		EQU 0900H   		; Memory address that stores the pressed button
 LAST_BUTTON 	EQU 0902H
 
 
@@ -46,17 +46,17 @@ COR_PIXEL		EQU 0FF00H	; cor do pixel: vermelho em ARGB (opaco e vermelho no máx
 
 
 PLACE 1000H
-STACK 100H			; Espaço reservado para a pilha 
+STACK 100H				; Espaço reservado para a pilha 
 
 STACK_INIT:
 
-DEF_NAVE:			; tabela que define o boneco (cor, largura, pixels)
+DEF_NAVE:				; tabela que define o boneco (cor, largura, pixels)
 	WORD ALTURA,LARGURA
 	WORD COR_PIXEL, 0, COR_PIXEL, 0, COR_PIXEL, COR_PIXEL, COR_PIXEL, COR_PIXEL, COR_PIXEL, COR_PIXEL		
 	
-SHIP_PLACE:			; Reference to the position of ship
-	WORD 101EH		; First byte refers the line and the second one the column
-
+SHIP_PLACE:				; Reference to the position of ship
+	WORD 101EH			; First byte refers the line and the second one the column
+	
 PEN_MODE:
 	WORD 0H
 
@@ -73,30 +73,30 @@ PLACE 0H
 Initializer:
 	MOV SP, STACK_INIT
 	MOV R0, 0 
-	MOV  [DEL_WARNING], R0			; apaga o aviso de nenhum cenário selecionado (o valor de R0 não é relevante)
-	MOV  [DEL_SCREEN], R0			; apaga todos os pixels já desenhados (o valor de R0 não é relevante)
-    	MOV  [SELECT_BACKGROUND], R0		; seleciona o cenário de fundo
+	MOV [DEL_WARNING], R0		; apaga o aviso de nenhum cenário selecionado (o valor de R0 não é relevante)
+	MOV [DEL_SCREEN], R0		; apaga todos os pixels já desenhados (o valor de R0 não é relevante)
+    	MOV [SELECT_BACKGROUND], R0	; seleciona o cenário de fundo
 
 Obtem_nave:
-	MOV R8, SHIP_PLACE			; recebe o a linha e coluna em que a nave se encontra(0-7 bits para coluna , 8-15 bits para linha)
-	MOV R9, DEF_NAVE 			; Recebe formato do boneco
-	CALL placement				; Calcula e guarda valores da linha e coluna em R1 E R2 respetivamente
-	CALL apaga_boneco			; Apaga o boneco do display
-	CALL desenha_boneco			; Desenha boneco
+	MOV R8, SHIP_PLACE		; recebe o a linha e coluna em que a nave se encontra(0-7 bits para coluna , 8-15 bits para linha)
+	MOV R9, DEF_NAVE 		; Recebe formato do boneco
+	CALL placement			; Calcula e guarda valores da linha e coluna em R1 E R2 respetivamente
+	CALL apaga_boneco		; Apaga o boneco do display
+	CALL desenha_boneco		; Desenha boneco
 
 MAIN_CYCLE:
-	CALL keypad;
-	CALL COMMANDS;
-	CALL METEOROS;
-	CALL MOV_NAVE;
-	JMP MAIN_CYCLE;
+	CALL keypad
+	CALL COMMANDS
+	CALL METEOROS
+	CALL MOV_NAVE
+	JMP MAIN_CYCLE
 
 COMMANDS:
-	RET;
+	RET
 
 METEOROS:
 	RET
-		;MOVIMENTOS EM RELACAO A NAVE
+					;MOVIMENTOS EM RELACAO A NAVE
 
 
 ;********************************************************************************************************
@@ -117,12 +117,12 @@ MOV_NAVE:
 
 MOVE_RIGHT:
 	MOV R7, 1
-	MOV [ALT_COL] , R7
+	MOV [ALT_COL], R7
 	JMP MOVE
 
 MOVE_LEFT:
 	MOV R7, -1
-	MOV [ALT_COL] , R7
+	MOV [ALT_COL], R7
 	JMP MOVE
 	
 MOVE:
@@ -132,8 +132,8 @@ MOVE:
 	CMP R7, 0
 	JZ NAVE_END
 	CALL apaga_boneco		; apaga o boneco
-	ADD R2 , R7			; adiciona o incremento a coluna para indicar a nova coluna onde comeca
-	ADD R8 , 1			;
+	ADD R2, R7			; adiciona o incremento a coluna para indicar a nova coluna onde comeca
+	ADD R8, 1			;
 	MOVB [R8], R2			; muda a coluna onde o objeto esta
 	CALL desenha_boneco		; desenha a nova posicao do objeto
 	CALL atraso			;
@@ -153,9 +153,9 @@ NAVE_END:
 
 apaga_boneco:
 	PUSH R6
-	MOV R6 , ERASER
-	MOV [PEN_MODE],R6 ;
-	CALL escreve_boneco;
+	MOV R6, ERASER
+	MOV [PEN_MODE], R6
+	CALL escreve_boneco
 	POP R6
 	RET
 
@@ -167,8 +167,8 @@ apaga_boneco:
 desenha_boneco:
 	PUSH R6
 	MOV R6 , PEN
-	MOV [PEN_MODE],R6 ;
-	CALL escreve_boneco;
+	MOV [PEN_MODE], R6 
+	CALL escreve_boneco
 	POP R6
 	RET
 
@@ -179,12 +179,11 @@ desenha_boneco:
 
 placement:
 	PUSH R8
-	MOVB R1 , [R8]			; R1 guarda linha
-	ADD R8 , 1			; Obtem endereço da coluna 
+	MOVB R1, [R8]			; R1 guarda linha
+	ADD R8, 1			; Obtem endereço da coluna 
 	MOVB R2, [R8]			; R2 stores the column
 	POP R8
 	RET
-	
 	
 	
 ;*******************************************************************************************	
@@ -192,13 +191,13 @@ placement:
 ;*******************************************************************************************
 
 escreve_boneco:
-	PUSH  	R0
-	PUSH 	R1
-	PUSH 	R3
-	PUSH	R9
-	MOV	R0, [R9]		; obtém a ALTURA do boneco
-	ADD	R9, 2			; adiciona 2 para obter o endereço da LARGURA do boneco
-	MOV	R3, [R9]		; obtém a LARGURA
+	PUSH R0
+	PUSH R1
+	PUSH R3
+	PUSH R9
+	MOV R0, [R9]			; obtém a ALTURA do boneco
+	ADD R9, 2			; adiciona 2 para obter o endereço da LARGURA do boneco
+	MOV R3, [R9]			; obtém a LARGURA
 	ADD R9, 2			; para obter a primeira cor
 
 escreve_linha:				; escreve linha de pixeis
@@ -224,13 +223,13 @@ escreve_coluna:       			; desenha os pixels do boneco(colunas) a partir da tabe
 	PUSH R5				; cor do pixel
 	
 escreve_pixeis_coluna:				
-	MOV	R5, [R9]		; cor para apagar o próximo pixel do boneco
+	MOV R5, [R9]			; cor para apagar o próximo pixel do boneco
 	CALL escolhe_cor		; determina a cor do pixel
-	CALL  escreve_pixel		; escreve cada pixel do boneco
+	CALL escreve_pixel		; escreve cada pixel do boneco
 	ADD R9, 2			; obter proxima cor
-    	ADD  R2, 1              	; próxima coluna
-    	SUB  R3, 1			; menos uma coluna para tratar
-   	JNZ  escreve_pixeis_coluna      ; continua até percorrer toda a largura do objeto
+    	ADD R2, 1          	    	; próxima coluna
+    	SUB R3, 1			; menos uma coluna para tratar
+   	JNZ escreve_pixeis_coluna      	; continua até percorrer toda a largura do objeto
   	POP R5							
    	POP R2							
    	POP R3
@@ -242,21 +241,21 @@ escreve_pixeis_coluna:
     
 escolhe_cor:
 	PUSH R6
-	MOV R6 , [PEN_MODE]
+	MOV R6, [PEN_MODE]
 	CMP R6,	ERASER 			; Checks if its in erasing mode
 	JNZ end_cor			; Se nao for Erasing a cor do pixel is a do objeto
 	MOV R5, ERASER			; ITS erasing so a cor de R5 is 0
 
 end_cor:
 	POP R6
-	RET							;END routine
+	RET				; END routine
    
    
     
 escreve_pixel:
-	MOV  [DEF_LINE], R1		; seleciona a linha
-	MOV  [DEF_COL], R2		; seleciona a coluna
-	MOV  [DEF_PIXEL], R5	; altera a cor do pixel na linha e coluna já selecionadas
+	MOV [DEF_LINE], R1		; seleciona a linha
+	MOV [DEF_COL], R2		; seleciona a coluna
+	MOV [DEF_PIXEL], R5		; altera a cor do pixel na linha e coluna já selecionadas
 	RET
 	
 	
@@ -278,23 +277,23 @@ keypad:
 	PUSH R2				; Locks R2 value
 	PUSH R3				; Locks R3 value
 	PUSH R4				; Locks R4 value
-	MOV  R1, LINE       ; First line to test 
-    MOV  R2, KEY_LIN	; Keypad input in R2
-	MOV	 R3, KEY_COL	; Keypad output in R3
+	MOV R1, LINE     	 	; First line to test 
+ 	MOV R2, KEY_LIN			; Keypad input in R2
+	MOV R3, KEY_COL			; Keypad output in R3
 
-check_keypad:			; Checks if there is a pressed button
-    MOVB [R2], R1      	; Injects Line in Keypad Lines
-    MOVB R0, [R3]      	; Reads from Keypad Colums
-    MOV  R4, KEY_MASK	; Loads Keypad Mask to
-    AND  R0, R4   		; Isolates the Lower Nibble
-    JZ 	 wait_button    ; Jumps if no button is pressed in that line
-	CALL button_calc	; Calls the process that calculates the button pressed
-	JMP keypad_end		; Jumps to the end
+check_keypad:				; Checks if there is a pressed button
+   	MOVB [R2], R1      		; Injects Line in Keypad Lines
+   	MOVB R0, [R3]      		; Reads from Keypad Colums
+   	MOV  R4, KEY_MASK		; Loads Keypad Mask to
+  	AND  R0, R4   			; Isolates the Lower Nibble
+  	JZ wait_button    		; Jumps if no button is pressed in that line
+   	CALL button_calc		; Calls the process that calculates the button pressed
+  	JMP keypad_end			; Jumps to the end
 
 wait_button:	
 	SHR R1, 1		   	; Changes which line is checked
 	JNZ check_keypad		; Jumps if there is still a line to check
-	MOV R1 , [BUTTON]		;
+	MOV R1, [BUTTON]		;
 	MOV [LAST_BUTTON], R1		; 
 	MOV R2, 0FFFFH 			; Moves value -1 (estado normal do botao) to R2
 	MOV [BUTTON], R2		; Changes BUTTON value to FH
@@ -319,18 +318,18 @@ calc_lin:				; Determines which line is being pressed (0-3)
 	SHR R1, 1		   	; Shifts the pressed line 1 bit to the right
 	JZ calc_col		   	; Jumps to calculate which column is being pressed
 	ADD R2, 1		   	; Adds 1 to the line counter
-	JMP calc_lin		; Repeats calc_lin keeps calculating the line
+	JMP calc_lin			; Repeats calc_lin keeps calculating the line
 
 calc_col:				; Determines which column is being pressed (0-3)
 	SHR R0, 1		   	; Shifts the pressed column 1 bit to the right
-	JZ button_calc_end	; Jumps to end routine
+	JZ button_calc_end		; Jumps to end routine
 	ADD R3, 1		   	; Adds 1 to the column counter
-	JMP calc_col		; Repeats calc_col to keep calculating the column counter
+	JMP calc_col			; Repeats calc_col to keep calculating the column counter
 
 button_calc_end:	
-	CALL button_formula	; Calculates the button that is being pressed
-	MOV R3 , [BUTTON]		;
-	MOV [LAST_BUTTON], R3	; 
+	CALL button_formula		; Calculates the button that is being pressed
+	MOV R3, [BUTTON]		;
+	MOV [LAST_BUTTON], R3		; 
 	MOV [BUTTON], R2		; Stores button pressed address in R0
 	RET
 
@@ -350,11 +349,11 @@ button_formula:
 ;***************************************************************************************	
 
 atraso:
-	PUSH	R11
+	PUSH R11
 	MOV R11, MOV_TIMER
 	
 ciclo_atraso:
-	SUB	R11, 1
-	JNZ	ciclo_atraso
-	POP	R11
+	SUB R11, 1
+	JNZ ciclo_atraso
+	POP R11
 	RET
